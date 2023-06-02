8ab777f2372b5c4619ad9b4f8a89a0ba594027ae
Beatriz <bia906@gmail.com>
2023-06-02 16:10:40 -0300

    subscriber.py

--- subscriber.py
+++ subscriber.py
@@ -8,14 +8,11 @@ HOST = '127.0.0.1'          # The remote host
 PORT = 5050            # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    # choice = input()                    # input no console
-    # s.sendall(bytes(choice, 'utf-8'))   # transforma string em bytestream
 
     is_subscriber = b'subscriber'
     s.sendall(is_subscriber)
     topic = input("[INSERT TOPIC NAME] ")
     s.send(bytes(topic, 'utf-8'))
     while True:
-        msg = input("[DIGITE UMA MENSAGEM DE TESTE: ]")
-        s.send(bytes(msg, 'utf-8'))
-        
+        data = s.recv(1024)
+        print(bytes(data))
